4ad394a48889d1599c649b46841762748761a3bc
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2025-02-18 15:59:21 +0300

    Initial commit

---
+++ StickerSmash/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View  >
+      <Text className=" text-blue-600 text-lg font-semibold">
+        What do we have here?
+      </Text>
+    </View>
+  );
+}
