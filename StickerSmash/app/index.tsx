0d71cf4873a6f21d8df122cfab98bd98a92e6fd2
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2025-02-19 11:10:57 +0300

    No progress

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,11 +1,31 @@
-import { Text, View } from "react-native";
 
-export default function Index() {
+
+import React from 'react';
+import {View, Text, Image, ScrollView, TextInput} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App = () => {
   return (
-    <View  >
-      <Text className=" text-blue-600 text-lg font-semibold">
-        What do we have here?
-      </Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={{height:'100%', margin:'20'}}>
+        <Text className="text-orange-500 text-2xl font-semibold p-8">Lesson on Components</Text>
+        <View className="grid text-center items-center">
+          <Text className="font-bold">Paws Crypto</Text>
+          <Image
+            source={{
+              uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+            }}
+            style={{width: 200, height: 200}}
+          />
+        </View>
+        <TextInput
+          className="border-2 border-green-400 rounded-xl m-4 px-4"
+          defaultValue="You can type in me"
+          
+          />
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
